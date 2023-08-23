4afe7a8d1f627a4fce4932471bda47b9e8ba03ad
Basston97 <133507074+Basston97@users.noreply.github.com>
2023-08-23 22:19:37 +0200

    homework8

---
+++ Homework8/Homework_8.py
@@ -0,0 +1,37 @@
+class Auto:
+    def __init__(self, brand, age, color, mark, weight):
+        self.brand = brand
+        self.age = age
+        self.color = color
+        self.mark = mark
+        self.weight = weight
+    
+    def move(self):
+        print(f"The {self.brand} {self.mark} is moving.")
+    
+    def stop(self):
+        print(f"The {self.brand} {self.mark} has stopped.")
+    
+    def birthday(self):
+        self.age += 1
+
+class Truck(Auto):
+    def __init__(self, max_load):
+        super(move).__init__
+    
+class Car(Auto):
+    pass
+# Пример использования класса
+my_auto = Auto(brand="Volkswagen", age=40, color="Grean", mark="Golf II", weight=900)
+print(f"Brand: {my_auto.brand}")
+print(f"Age: {my_auto.age}")
+print(f"Color: {my_auto.color}")
+print(f"Mark: {my_auto.mark}")
+print(f"Weight: {my_auto.weight}")
+
+my_auto.move()
+my_auto.stop()
+
+print("Current age:", my_auto.age)
+my_auto.birthday()
+print("New age after birthday:", my_auto.age)
